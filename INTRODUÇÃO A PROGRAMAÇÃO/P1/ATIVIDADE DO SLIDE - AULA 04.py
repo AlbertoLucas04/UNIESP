034d1660156f2ddfcaea1cb5a5b133057c36ac67
AlbertoLucas04 <ABERTOLUCAS12@GMAIL.COM>
2024-09-02 15:45:05 -0300

    ATIVIDADE DO SLIDE - AULA 04.py

---
+++ INTRODUÇÃO A PROGRAMAÇÃO/P1/ATIVIDADE DO SLIDE - AULA 04.py
@@ -0,0 +1,7 @@
+print('Olá, seja bem-vindo a Casa de Festas!')
+
+idade = int(input('Digite sua idade: ')
+  if idade <= 17
+      print('Volte para casa!')
+  elif idade >= 18
+      print('Entre e aproveite!')
