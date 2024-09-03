8083eb726e2089c14066dc0a8f4fd4c0f2cf34a0
AlbertoLucas04 <ABERTOLUCAS12@GMAIL.COM>
2024-09-03 17:39:50 -0300

    Delete INTRODUÇÃO A PROGRAMAÇÃO/P1/exemplo2_aula04.py

--- INTRODUÇÃO A PROGRAMAÇÃO/P1/exemplo2_aula04.py
+++
@@ -1,9 +0,0 @@
-dia = 1
-match dia:
-    case 1:
-        print("Dia útil")
-    case 2: 
-        print("Final de semana ou feriado!")    
-
-    case _: 
-        print(f"Valor {dia} inválido")        
