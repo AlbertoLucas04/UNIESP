99629dce77a8e5d9d1f879ca9e459f66d95e9285
AlbertoLucas04 <ABERTOLUCAS12@GMAIL.COM>
2024-09-05 17:18:13 -0300

    Delete INTRODUÇÃO A PROGRAMAÇÃO/P1/AULA 04/atividade_aula04_Q2.py

--- INTRODUÇÃO A PROGRAMAÇÃO/P1/AULA 04/atividade_aula04_Q2.py
+++
@@ -1,8 +0,0 @@
-# QUESTÃO 02
-
-number = float(input('Digite um número: '))
-
-if number >= float('50'):
-    print('Verificação realizada. Número maior!')
-else:
-    print('Verificação realizada. Número menor!')
