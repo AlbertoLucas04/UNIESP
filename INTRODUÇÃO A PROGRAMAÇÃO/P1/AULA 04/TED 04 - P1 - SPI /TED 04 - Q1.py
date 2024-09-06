f70f4dbdb528c398c26136b9d157e65832481518
AlbertoLucas04 <ABERTOLUCAS12@GMAIL.COM>
2024-09-06 10:39:06 -0300

    Create TED 04 - Q1.py

---
+++ INTRODUÇÃO A PROGRAMAÇÃO/P1/AULA 04/TED 04 - P1 - SPI /TED 04 - Q1.py
@@ -0,0 +1,11 @@
+# QUESTÃO 01
+
+msg = ('Olá, seja bem vindo ao seu verificador numérico!')
+print(msg)
+
+number = int(input('Insira um número: )
+
+if number > 0:
+  print('O seu número é Positivo!')
+elif number < 0:
+  print('O seu número é negativo!')                   
