fa6b720c138d716db1cafbc5ba4791baea151671
AlbertoLucas04 <ABERTOLUCAS12@GMAIL.COM>
2024-09-11 18:01:33 -0300

    Update q12.py

--- INTRODUÇÃO A PROGRAMAÇÃO/P1/AULA 05/q12.py
+++ INTRODUÇÃO A PROGRAMAÇÃO/P1/AULA 05/q12.py
@@ -1,14 +1,20 @@
 # QUESTÃO 12
 
-at_espada = float(input('Insira o ataque da sua espada: '))
-dur_espada = float(input('Insira a durabilidade da sua espada: '))
+at_e = float(input('Insira o ataque da sua espada: '))
+dur_e = float(input('Insira a durabilidade da sua espada: '))
 
-at_arco = float(input('Insira o ataque do seu arco: '))
-dur_arco = float(input('Insira a durabilidade do seu arco: '))
+at_a = float(input('Insira o ataque do seu arco: '))
+dur_a = float(input('Insira a durabilidade do seu arco: '))
 
-at_lança = float(input('Insira o ataque da sua lança: '))
-dur_lança = float(input('Insira a durabilidade da sua lança: '))
+at_l = float(input('Insira o ataque da sua lança: '))
+dur_l = float(input('Insira a durabilidade da sua lança: '))
 
-if at_espada > at_arco and at_espada > at_arco:
-    print
+if at_e > 50 and dur_e > 70:
+    print('Você deve escolher a Espada!')
+elif at_a > 50 and dur_a > 70:
+    print('Você deve escolher o Arco!')
+elif at_l > 50 and dur_l > 70:
+    print('Você deve escolher a Lança!')   
+else:   
+    print('Você deve trocar suas armas!')
 
