cc9fc5333be45992fa8e528f404839aec971628c
AlbertoLucas04 <ABERTOLUCAS12@GMAIL.COM>
2024-09-17 17:57:30 -0300

    ted 06

---
+++ INTRODUÇÃO A PROGRAMAÇÃO/P1/AULA 06/ted_06.py
@@ -0,0 +1,7 @@
+# TED 06
+
+name = {}
+
+vingadores = ('Capitão América' , 'Homem de Ferro' , 'Viúva Negra' , 'Thor' , 'Hulk')
+for name in vingadores:
+    print(f'Olá, {name}, você está convidado(a) para a minha festa!')
