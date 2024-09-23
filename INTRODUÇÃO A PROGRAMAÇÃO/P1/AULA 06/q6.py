9073a335557aee2965d3849478bd0868dd3e9bcd
Alberto Lucas <ABERTOLUCAS12@GMAIL.COM>
2024-09-23 16:00:44 -0300

    Atualização do código da Q6
    Código antigo:
    
    celebridades = ['Whindersson Nunes' , 'Tirulipa' , 'Tiririca' , 'Igor Guedes' , 'Douglas di Lima', 'Renan da Resenha', 'Messias Batista']
    
    for c in celebridades:
        print(f'Olá, {c}, você está convidado para jantar na minha casa, dia 22/09, às 16:00!')
    
        for rc in celebridades:
            celebridades.pop()
            print('Pessoal, o Messias não poderá ir, pois está sofrendo pelo Vasco!')
            print(f'Olá, {rc}, permanece convidado para jantar na minha casa, dia 22/09, às 16:00!')

--- INTRODUÇÃO A PROGRAMAÇÃO/P1/AULA 06/q6.py
+++ INTRODUÇÃO A PROGRAMAÇÃO/P1/AULA 06/q6.py
@@ -1,11 +1,21 @@
+import time
+
 celebridades = ['Whindersson Nunes' , 'Tirulipa' , 'Tiririca' , 'Igor Guedes' , 'Douglas di Lima', 'Renan da Resenha', 'Messias Batista']
 
 for c in celebridades:
-    print(f'Olá, {c}, você está convidado para jantar na minha casa, dia 22/09, às 16:00!')
+    print(f'Olá, {c}, você está convidado para jantar na minha casa, dia 29/09, às 18:30!')
+    time.sleep(1)
+    print()   
 
-    for rc in celebridades:
-        celebridades.pop()
-        print('Pessoal, o Messias não poderá ir, pois está sofrendo pelo Vasco!')
-        print(f'Olá, {rc}, permanece convidado para jantar na minha casa, dia 22/09, às 16:00!')
+print()
 
-    
+print('Pessoal, o Messias não poderá ir, pois estará sofrendo pelo Vasco!')
+
+print()
+print()
+
+confirmados = ['Whindersson Nunes' , 'Tirulipa' , 'Tiririca' , 'Igor Guedes' , 'Douglas di Lima', 'Renan da Resenha']  
+for rc in confirmados:
+    print(f'{rc}, diferente do sofredor, você permanece convidado para jantar na minha casa, dia 29/09, às 18:30!')
+    time.sleep(1)
+    print()    
