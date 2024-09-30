b9c2a0d6a767876c37b231bd4afd8ab3e7d9e8b5
AlbertoLucas04 <abertolucas12@gmail.com>
2024-09-30 20:17:50 -0300

    correção de código

--- introducao_a_programacao/P1/AULA_07/lista07_q6.py
+++ introducao_a_programacao/P1/AULA_07/lista07_q6.py
@@ -5,10 +5,10 @@ print('Olá, seja bem-vindo(a) ao Verificador de Números Primos!')
 
 num = int(input('Insira um número: '))
 
-for primo in range(2, int(num**0.5) + 1):
+for primo in range(2, int(num**0.5 + 1)):
     if num % primo == 0:
         print(f'O número {num} não é primo!')
-        break
+             
     else:
         print(f'O número {num} é primo!')
-        break
+        
