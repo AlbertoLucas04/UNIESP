f6dbfece2a3e85c34d16fda97c4d8ac4e811b972
AlbertoLucas04 <abertolucas12@gmail.com>
2024-10-14 18:34:38 -0300

    continuação

--- introducao_a_programacao/P1/AULA_07/lista07_q7.py
+++ introducao_a_programacao/P1/AULA_07/lista07_q7.py
@@ -1,8 +1,65 @@
 # QUESTÃO 07
 
-# Fatorial = n * n-1 * n-2 * ... * n - (n-1)
-
 p_n = int(input('Insira um número para descobrir o seu fatorial: '))
+
+
+for i in range(p_n - 1, 0, -1):
+    print(i)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+        
+    
+
+    
+    
+    
+    
+   
+    
+    
+    
+
 
 
 
