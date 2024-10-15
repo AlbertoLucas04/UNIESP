255eb2f442c906a9c1ba091e5a43597e5f4f04f3
Alberto Lucas <ABERTOLUCAS12@GMAIL.COM>
2024-10-15 08:53:49 -0300

    alteração do for

--- introducao_a_programacao/P1/AULA_07/lista07_q7.py
+++ introducao_a_programacao/P1/AULA_07/lista07_q7.py
@@ -2,9 +2,23 @@
 
 p_n = int(input('Insira um número para descobrir o seu fatorial: '))
 
+if p_n < 0:
+    print('Insira um valor positivo!')
 
-for i in range(p_n - 1, 0, -1):
-    print(i)
+m1 = 1
+m2 = 2
+
+for _ in range(1, p_n+1):
+    p_m = m1 * m2
+    print(f'{p_n}! = {p_m}')
+    
+
+    m1 = m2
+    m2 = p_m
+    
+    
+    
+
 
 
     
