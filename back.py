a022a16a82bddac4b92df5a51ebe47ee2b15e16c
Alberto Lucas Martins Alves <AlbertoLucas@MacBook-Air-de-Ingrid.local>
2025-01-30 00:09:57 -0300

    Back to the work, dude! 30.01.2025

---
+++ back.py
@@ -0,0 +1,18 @@
+def multiplicar (x, y):
+    m = x * y
+    return m
+
+variavel_num = 19
+
+variavel_str = ("Do something about...")
+
+variavel_flt = 19.1
+
+a = float(input("Digite um número: "))
+
+b = float(input("Digite um número: "))
+
+m1 = multiplicar(a, b)
+
+print(m1)
+
