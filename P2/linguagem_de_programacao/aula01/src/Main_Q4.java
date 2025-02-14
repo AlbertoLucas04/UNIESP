ba9faf2d517e30b4a3f6e6f934a811058633c2c0
AlbertoLucas04 <abertolucas12@gmail.com>
2025-02-13 23:06:49 -0300

    Finalização da q4

--- P2/linguagem_de_programacao/aula01/src/Main_Q4.java
+++ P2/linguagem_de_programacao/aula01/src/Main_Q4.java
@@ -2,9 +2,22 @@ public class Main_Q4 {
 
     public static void main(String[] args) {
 
-        double salario = 2500.39;
+        double salario = 0;
+        double desc_1 = salario - (0.05 * salario);
+        double desc_2 = salario - (0.10 * salario);
+        double desc_3 = salario - (0.20 * salario);
 
-        System.out.println(salario);
+        if (salario <= 900) {
+            System.out.println("\nVocê é isento de Imposto de Renda!\n");
+        } else if (salario > 900 && salario <= 1501 ) {
+            System.out.println("\nVocê terá um desconto de 05% no seu salário referente ao Imposto de Renda, totalizando R$ " + desc_1);
+            
+        } else if (salario > 1501 && salario <= 2500) {
+            System.out.println("\nVocê terá um desconto de 10% no seu salário referente ao Imposto de Renda, totalizando R$ " + desc_2 );
+        }
+        else{
+            System.out.println("\nVocê terá um desconto de 20% no seu salário referente ao Imposto de Renda, totalizando R$ " + desc_3 );
+        }
         
     }
 }
